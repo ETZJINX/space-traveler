af04e628377e0eacca975fabfcf33fa268de36f6
ERFAN TOHIDI <etzjinx@gmail.com>
2026-07-09 17:11:50 +0330

    finishing the render part and ready for end of project

--- WinFormsApp1/Logics/Render.cs
+++ WinFormsApp1/Logics/Render.cs
@@ -4,7 +4,44 @@ using System.Text;
 
 namespace WinFormsApp1.Logics
 {
-    internal class Render
+    public class Render
     {
+        public static void drawplayer(Graphics g,player player2)
+        {
+            g.FillRectangle(Brushes.Red, player2.X, player2.Y, player2.Width, player2.Height);
+        }
+    
+        public static void drawenemy(Graphics g, template template)
+        {
+            if (template is StandardEnemy)
+            {
+                g.FillRectangle(Brushes.Blue, template.X, template.Y, template.Width, template.Height);
+            }
+            if (template is ScoutEnemy)
+            {
+                g.FillRectangle(Brushes.Green, template.X, template.Y, template.Width, template.Height);
+            }
+            if (template is ShooterEnemy)
+            {
+                g.FillRectangle(Brushes.Yellow, template.X, template.Y, template.Width, template.Height);
+            }
+            if (template is HeavyTankEnemy)
+            {
+                g.FillRectangle(Brushes.Orange, template.X, template.Y, template.Width, template.Height);
+            }
+            if (template is TerroristEnemy)
+            {
+                g.FillRectangle(Brushes.Brown, template.X, template.Y, template.Width, template.Height);
+            }
+        }
+        public static void drawbullet(Graphics g,Bullet bullet)
+        {
+            g.FillRectangle(Brushes.Gray, bullet.X, bullet.Y, bullet.Width, bullet.Height);
+        }
+        public static void drawcoin(Graphics g,Coindrop coin)
+        {
+            g.FillEllipse(Brushes.DarkGoldenrod, coin.X, coin.Y, 10, 10);
+        }
+    
     }
 }
